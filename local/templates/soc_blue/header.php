8ca89d46689e04ebeab1a10a7872a33d36eca659
kilowat <kadantsev.sergey@gmail.com>
2020-03-10 12:23:28 +0300

    work

--- local/templates/soc_blue/header.php
+++ local/templates/soc_blue/header.php
@@ -2,7 +2,6 @@
 <html lang="ru">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300, 400,400i,700&display=swap&subset=cyrillic" rel="stylesheet">
     <link rel="shortcut icon" href="favicon/favicon.ico" />
     <title><?=$APPLICATION->ShowTitle()?></title>
     <?
